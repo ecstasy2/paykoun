1c55f246021dac3ab3b991584a5b79dea71cae34
Bobo Diallo <bobo.diaraye@gmail.com>
2016-09-06 06:01:40 -0700

    Linting cleanup

--- test/lib/paykoun.spec.js
+++ test/lib/paykoun.spec.js
@@ -1,122 +1,95 @@
-'use strict';
+/* eslint-disable no-unused-expressions */
+/* global it, describe, beforeEach */
 
-var paykounPath = '../../lib/paykoun';
+const paykounPath = '../../lib/paykoun';
 
 
-var rewire = require('rewire'); // rewiring library
+const rewire = require('rewire'); // rewiring library
 
-var PaykounContext = rewire( '../../lib/context');
+// const PaykounContext = rewire('../../lib/context');
 
-var chai = require('chai'); // assertion library
-var expect = chai.expect;
-var assert = chai.assert;
-var should = chai.should;
-var sinon = require('sinon');
-var util = require('util');
-var vasync = require('vasync');
-var _ = require('lodash');
-var sinonChai = require('sinon-chai');
-var MockMgr = require('./mock/ikue');
+const chai = require('chai');
+const vasync = require('vasync');
+const _ = require('lodash');
+const sinonChai = require('sinon-chai');
+const MockMgr = require('./mock/ikue');
+
+const expect = chai.expect;
+const assert = chai.assert;
+
 chai.use(sinonChai);
 
-var Paykoun = rewire(paykounPath);
+const Paykoun = rewire(paykounPath);
 
-var WorkQueueMgr = require('ikue').WorkQueueMgr;
-
-
-function waitForQueues(arg, done){
-
-  var queues = arg;
+function waitForQueues(arg, done) {
+  let queues = arg;
 
   if (!_.isArray(arg)) {
     queues = [arg];
-  };
+  }
 
   vasync.forEachParallel({
-    func: function(queue, callback){
-      queue.on('ready', function(){
+    func: (queue, callback) => {
+      queue.on('ready', () => {
         callback();
       });
     },
-    inputs: queues
-  }, function(err, res){
+    inputs: queues,
+  }, (err, res) => {
     done(err, res);
   });
 }
 
-describe('Paykoun', function(){
-  var queueMgr;
+describe('Paykoun', () => {
+  let queueMgr;
 
-  beforeEach(function(){
+  beforeEach(() => {
     queueMgr = new MockMgr();
+  });
 
-    Paykoun.__set__("PaykounContext", PaykounContext);
-  })
-
-  describe('PaykounContext', function(){
-
-
-    beforeEach(function(){
-    });
-
-    it('Should create context correctly', function(){
-      var context = Paykoun.createContext(queueMgr);
+  describe.only('PaykounContext', () => {
+    it('Should create context correctly', () => {
+      const context = Paykoun.createContext(queueMgr);
       expect(context.registerWorker).to.exist;
     });
 
-    it('Running a context should create work queues', function(done){
-      var context = Paykoun.createContext(queueMgr);
-      expect(context.registerWorker).to.exist;
-
-      var triggers = ['event5', 'event4', 'event3'];
-      var setWorkQueueSpy = sinon.spy(function(queue){
-        queue.triggers = triggers;
-        this.workQueue = queue;
-      });
-
-      var fakeWorkFunc = function(job, done){
-        var util = require('util');
+    it('Running a context should create work queues', (done) => {
+      const context = Paykoun.createContext(queueMgr);
+      const fakeWorkFunc = () => {
       }
 
-      context.registerWorker(Paykoun.createWorker("Worker1", {
+      context.registerWorker(Paykoun.createWorker('Worker1', {
         isolationPolicy: 'thread',
         concurrency: 1,
         triggers: ['event1'],
-        work: fakeWorkFunc
+        work: fakeWorkFunc,
       }));
 
-      context.registerWorker(Paykoun.createWorker("Worker2", {
+      context.registerWorker(Paykoun.createWorker('Worker2', {
         isolationPolicy: 'thread',
         concurrency: 1,
         triggers: ['event2'],
-        work: fakeWorkFunc
+        work: fakeWorkFunc,
       }));
 
-      context.run(function(err){
-        if (err) {
-
-        };
+      context.run(() => {
       });
 
-      queueMgr.on('ready', function(){
+      queueMgr.on('ready', () => {
         expect(queueMgr.queues.length).to.eql(2);
-        
-        var queue1 = queueMgr.queues[0];
 
-        expect(queue1.name).to.eql("Worker1");
+        const queue1 = queueMgr.queues[0];
 
-        waitForQueues(queueMgr.queues, function(err, res){
+        expect(queue1.name).to.eql('Worker1');
+
+        waitForQueues(queueMgr.queues, (err) => {
+          expect(err).to.be.null;
           assert.ok(queueMgr.queues[0].eventBus);
           assert.ok(queueMgr.queues[1].eventBus);
 
           done(err);
         });
       });
-
     });
-  
-    it('If connecting a queue failed, we should destroy all queues and no thread should be created');
-
-    it('If creating a thread pool failed, we should destroy all queues and destroy other thread pools');
   });
 });
