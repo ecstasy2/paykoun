72dc822468b441f52fbbb81dfce4a89721d7dfa9
Bobo Diallo <bobo.diaraye@gmail.com>
2016-09-08 00:57:42 -0700

    eslint-ing

--- test/lib/job_runner.spec.js
+++ test/lib/job_runner.spec.js
@@ -1,234 +1,69 @@
-'use strict';
-
-var paykounPath = '../../lib/';
-
-
-var rewire = require('rewire'); // rewiring library
-
-var chai = require('chai'); // assertion library
-var expect = chai.expect;
-var assert = chai.assert;
-var should = chai.should;
-var sinon = require('sinon');
-var util = require('util');
-var vasync = require('vasync');
-var _ = require('lodash');
-var sinonChai = require('sinon-chai');
-var EventEmitter = require('events').EventEmitter;
-var MockMgr = require('./mock/ikue');
-chai.use(sinonChai);
-
-var Paykoun = rewire(paykounPath + 'paykoun');
-var JobRunner = rewire(paykounPath + 'job_runner');
-
-function FakeBrowserify(inputStream){
-  function Construct(){
-    var self = this;
-
-    EventEmitter.call(this);
-
-    this.require = sinon.spy();
-
-    this.bundle = sinon.spy(function(){
-      setTimeout(function(){
-        self.emit('end');
-      }.bind(self), 5);
-
-      return self;
-    });
-
-    this.pipe = sinon.spy();
-  }
-
-  util.inherits(Construct, EventEmitter);
-
-  return new Construct();
-}
-
-function FakeThreadPool(){
-  var fakePool = {
-    size: 1,
-    all: {
-      error: null,
-      failOnIndex: false,
-      evalCallback: null,
-      eval: sinon.spy(function(code, callback){
-        fakePool.all.evalCallback = sinon.spy(callback);
-
-        process.nextTick(function(){
-          for (var i = 0; i < fakePool.size; i++) {
-            if (fakePool.all.failOnIndex && fakePool.all.failOnIndex == i) {
-              setTimeout(function(){
-                fakePool.all.evalCallback(fakePool.all.error || new Error('FakePool eval error'));
-              }, i*2);
-            } else {
-              setTimeout(function(){
-                fakePool.all.evalCallback(null);
-              }, i*2);
-            }
-          };
-        });
-      }),
-    },
-    any: {
-
-    },
-    destroy: sinon.spy()
-  };
-
-  fakePool.on = sinon.spy();
-
-  return fakePool;
-}
-
-describe('JobRunner', function(){
-  var queueMgr;
-
-  beforeEach(function(){
-    queueMgr = new MockMgr();
-  }),
-
-  describe('Basics', function(){
-    it('Creating a JobRunner validate the properties', function(){
-      assert.throws(JobRunner.create, /type/);
-      
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'vasync'}),
-        /concurrency/);
-
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'vasync', concurrency: 12}),
-        /name/);
-
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'vasync', concurrency: 12, name: 'Name'}),
-        /callback/);
-    });
-
-    it('Creating a JobRunner with invalid "type" fail', function(){
-      var done = sinon.spy();
-
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'unexisting', concurrency: 1, name: 'Name'}, done));
-
-      assert.doesNotThrow(_.bind(JobRunner.create, 
-        this, 
-        {type: 'vasync', concurrency: 1, name: 'Name'}, done));
-
-      assert.doesNotThrow(_.bind(JobRunner.create, 
-        this, 
-        {type: 'vasync', concurrency: 1, name: 'Name'}, done));
-    });
-  });
-
-  describe.skip('ThreadRunner', function(){
-
-    var FakeThreads
-      , fakePool;
-    beforeEach(function(){
-      fakePool = FakeThreadPool();
-
-      FakeThreads = {
-        createPool: sinon.stub().returns(fakePool)
-      };
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-underscore-dangle */
+/* global it, describe, beforeEach, afterEach */
 
 
-      JobRunner.__set__('Threads', FakeThreads);
-      JobRunner.__set__('fs', {
-        readFileSync: sinon.stub().returns('var hello;')
-      });
+const paykounPath = '../../lib/'
 
-      JobRunner.__set__('browserify', FakeBrowserify);
-    });
 
-    describe('Basics', function(){
-    it('Creating a JobRunner validate the properties', function(){
-      assert.throws(JobRunner.create, /type/);
-      
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'thread'}),
-        /concurrency/);
+const rewire = require('rewire') // rewiring library
 
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'thread', concurrency: 12}),
-        /name/);
+const chai = require('chai')
+const sinon = require('sinon')
+const util = require('util')
+const _ = require('lodash')
+const sinonChai = require('sinon-chai')
+const EventEmitter = require('events').EventEmitter
+const MockMgr = require('./mock/ikue')
 
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'thread', concurrency: 12, name: 'Name'}),
-        /callback/);
-    });
+const assert = chai.assert
+chai.use(sinonChai)
 
-    it('Creating a JobRunner with invalid "type" fail', function(){
-      var done = sinon.spy();
+// const Paykoun = rewire(`${paykounPath}/paykoun`)
+const JobRunner = rewire(`${paykounPath}/job_runner`)
 
-      assert.throws(_.bind(JobRunner.create, 
-        this, 
-        {type: 'unexisting', concurrency: 1, name: 'Name'}, done));
+describe('JobRunner', () => {
+  let queueMgr = null
 
-      assert.doesNotThrow(_.bind(JobRunner.create, 
-        this, 
-        {type: 'thread', concurrency: 1, name: 'Name'}, done));
+  beforeEach(() => {
+    queueMgr = new MockMgr()
+  })
 
-      assert.doesNotThrow(_.bind(JobRunner.create, 
-        this, 
-        {type: 'vasync', concurrency: 1, name: 'Name'}, done));
-    });
-  });
+  describe('Basics', () => {
+    it('Creating a JobRunner validate the properties', () => {
+      assert.throws(JobRunner.create, /type/)
 
-    it('Creating a JobRunner actually create threads and evaluate browserified code', function(done){
-      var verify;
+      assert.throws(_.bind(JobRunner.create,
+        this,
+        { type: 'vasync' }),
+        /concurrency/)
 
-      var onDone = sinon.spy(function(err){
-        verify();
-        done(err);
-      });
+      assert.throws(_.bind(JobRunner.create,
+        this,
+        { type: 'vasync', concurrency: 12 }),
+        /name/)
 
-      var createFunc = _.bind(JobRunner.create, 
-        this, {
-          type: 'thread', 
-          concurrency: 1, 
-          name: 'Name'}, onDone);
+      assert.throws(_.bind(JobRunner.create,
+        this,
+        { type: 'vasync', concurrency: 12, name: 'Name' }),
+        /callback/)
+    })
 
-      createFunc();
+    it('Creating a JobRunner with invalid "type" fail', () => {
+      const done = sinon.spy()
 
-      function verify(){
-        expect(onDone, "Done method should be called").to.have.been.calledOnce
-        expect(FakeThreads.createPool, "FakeThreads.createPool").to.have.been.calledWith(1);
+      assert.throws(_.bind(JobRunner.create,
+        this,
+         { type: 'unexisting', concurrency: 1, name: 'Name' }, done))
 
-        expect(fakePool.all.eval, "browserified code was evaluated inside the thread").to.have.been.called;
-      }
-    });
+      assert.doesNotThrow(_.bind(JobRunner.create,
+        this,
+        { type: 'vasync', concurrency: 1, name: 'Name' }, done))
 
-    it('Creating a JobRunner actually evaluate the code for each thread', function(done){
-      var verify;
-
-      var onDone = sinon.spy(function(err){
-        verify();
-        done(err);
-      });
-
-      var createFunc = _.bind(JobRunner.create, 
-        this, {
-          type: 'thread', 
-          concurrency: 4, 
-          name: 'Name'}, onDone);
-
-      fakePool.size = 4;
-
-      createFunc();
-
-      function verify(){
-        expect(fakePool.all.evalCallback, "code should be evaluated in each thread").to.have.been.callCount(4);
-      }
-    });
-
-    it('Creating a JobRunner fail if a thread fail to evaluate the code');
-  });
-});
+      assert.doesNotThrow(_.bind(JobRunner.create,
+        this,
+        { type: 'vasync', concurrency: 1, name: 'Name' }, done))
+    })
+  })
+})
