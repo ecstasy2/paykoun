393d67928153ecb75b8c24ee86c9803295726006
Bobo Diallo <bobo.diaraye@gmail.com>
2016-09-09 03:01:33 -0700

    eslinting

--- test/lib/helpers_support.js
+++ test/lib/helpers_support.js
@@ -1,42 +1,39 @@
-'use strict';
+/* global beforeEach, describe, it */
+/* eslint-disable no-unused-expressions */
 
-var paykounPath = '../../lib/paykoun';
+const paykounPath = '../../lib/paykoun';
 
-var chai = require('chai'); // assertion library
-var expect = chai.expect;
-var assert = chai.assert;
-var sinon = require('sinon');
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 
-var sinonChai = require('sinon-chai');
+const expect = chai.expect;
+const assert = chai.assert;
 chai.use(sinonChai);
 
-var Paykoun = require(paykounPath);
+const Paykoun = require(paykounPath);
 
-/* global beforeEach, describe */
-describe('Paykoun Test Context', function(){
 
-  var context = null;
-  var queue = null;
-  var sayHelloWorker = null;
-  var failingWorkerFunc = null;
-  var sayHelloSpy = null;
-  var funcSpy = null;
+describe('Paykoun Test Context', () => {
+  let context = null;
+  let queue = null;
+  let sayHelloWorker = null;
+  let failingWorkerFunc = null;
+  let sayHelloSpy = null;
+  let funcSpy = null;
 
-  beforeEach(function(done){
+  beforeEach((done) => {
     context = Paykoun.createTestContext();
     queue = context.queue();
 
     sayHelloSpy = sinon.spy();
     funcSpy = sinon.spy();
 
-    sayHelloWorker = sinon.spy(function(job, onJobDone){
-
-      var $sayHello = this.$getHelper('sayHello');
-
-      var $objectHelper = this.$getHelper('objectHelper');
+    sayHelloWorker = sinon.spy((job, onJobDone) => {
+      const $sayHello = this.$getHelper('sayHello');
+      const $objectHelper = this.$getHelper('objectHelper');
 
       $objectHelper.func();
-
       $sayHello(job.name);
 
       onJobDone(null, null);
@@ -44,19 +41,19 @@ describe('Paykoun Test Context', function(){
 
     context.useHelper('sayHello', sayHelloSpy);
     context.useHelper('objectHelper', {
-      func: funcSpy
+      func: funcSpy,
     });
 
     context.registerWorker(Paykoun.createWorker('SayHelloWorker', {
       triggers: ['sayHello'],
-      work: sayHelloWorker
+      work: sayHelloWorker,
     }));
 
-    failingWorkerFunc = sinon.spy(function(job, onJobDone){
+    failingWorkerFunc = sinon.spy((job, onJobDone) => {
       try {
         this.$getHelper('unexistingHelper');
-      } catch (e){
-        return onJobDone(e, null);
+      } catch (e) {
+        onJobDone(e, null);
       }
 
       onJobDone(null, null);
@@ -64,57 +61,55 @@ describe('Paykoun Test Context', function(){
 
     context.registerWorker(Paykoun.createWorker('FailingWorker', {
       triggers: ['failingTrigger'],
-      work: failingWorkerFunc
+      work: failingWorkerFunc,
     }));
 
     context.run(done);
   });
 
-  it('should allow us to use a registered helper', function(done){
+  it('should allow us to use a registered helper', (done) => {
     context.dontMock('sayHello');
 
-    queue.pushJob('sayHello', {name: 'Hello world'});
+    queue.pushJob('sayHello', { name: 'Hello world' });
 
-    queue.flush(function(){
-      expect(sayHelloSpy).to.have.been.called;
+    queue.flush(() => {
+      expect(sayHelloSpy).to.have.been.called
 
-      var helperCall = sayHelloSpy.firstCall;
+      const helperCall = sayHelloSpy.firstCall;
       expect(helperCall).to.not.have.thrown();
       expect(helperCall.args[0]).to.equal('Hello world');
       done();
     });
-
   });
 
   // Maybe in the long run we want to stub everything? or provide for a way that avoid
   // setting non function helpers?
-  it('should only stub function helpers', function(done){
-    queue.pushJob('sayHello', {name: 'Hello world'});
+  it('should only stub function helpers', (done) => {
+    queue.pushJob('sayHello', { name: 'Hello world' });
 
-    queue.flush(function(){
+    queue.flush(() => {
       expect(sayHelloSpy).to.have.been.called;
-      var helperCall = sayHelloSpy.firstCall;
+      const helperCall = sayHelloSpy.firstCall;
       expect(helperCall).to.not.have.thrown();
       expect(helperCall.args[0]).to.equal('Hello world');
       done();
     });
-
   });
 
-  it('should throw an error when trying to use an unregistered helper', function(done){
+  it('should throw an error when trying to use an unregistered helper', (done) => {
     context.dontMock('unexistingHelper');
 
-    queue.pushJob('failingTrigger', {name: 'Hello world'});
+    queue.pushJob('failingTrigger', { name: 'Hello world' });
 
-    queue.flush(function(){
+    queue.flush(() => {
       assert(failingWorkerFunc.called);
 
-      var onDoneCall = failingWorkerFunc.firstCall.args[1];
+      const onDoneCall = failingWorkerFunc.firstCall.args[1];
 
       assert(onDoneCall.calledOnce);
-      expect(onDoneCall.firstCall.args[0]).to.match(/Error: Trying to use an unregistered helper function/);
+      expect(onDoneCall.firstCall.args[0])
+        .to.match(/Error: Trying to use an unregistered helper function/);
       done();
     });
-
   });
 });
